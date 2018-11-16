abd20bfcf4a103e70189579abb794389f063cfa2
HP <4str90@gmail.com>
2018-11-16 12:13:38 +0300

    'les_2_ver_2.0'

--- BMI.py
+++ BMI.py
@@ -1,8 +1,8 @@
 # входные параметры
 name = input('Введите Ваше имя: ')
 age = int(input('Введите Ваш возраст: '))
-sex = input('Пол М/Ж: ').upper()
 
+sex = input('Пол М/Ж: ').upper()
 while True:
     if sex == 'М' or sex == 'Ж': # ждем от пользователя строго ввода "М" или "Ж"
         break
@@ -13,23 +13,26 @@ while True:
 weight = int(input('Введите Ваш вес в кг: '))
 height = int(input('Введите Ваш рост в см: '))
 greeting = ''
-recomendation = ''
-graph = '__________________=======+++++++++++++++'
+RECOMENDATION_BAD = 'Все очень плохо. И тут даже не до шуток. ' \
+                    'Рекомендуем повышение веса и лечение анерексии. ' \
+                    'ВЫСОКИЙ риск угрозы здоровью.'
+RECOMENDATION_NOT_BAD = 'ОТСУТСТВУЕТ риск для здоровья. '\
+                        'Однако все же непобходимо чуть-чуть больше кушать.'
+RFECOMENDATION_NORM = 'Все прекрасно. Можете сосредоточиться ' \
+                      'для достижения других жизненных целей)'
+RECOMENDATION_INCREASED = 'ПОВЫШЕННЫЙ риск для здоровья. ' \
+                          'Рекомендуется похудение.'
+RECOMENDATION_HIGHT = 'ВЫСОКИЙ риск для здоровья. ' \
+                      'Настоятельно рекомендуется снижение веса.'
+RECOMENDATION_VERY_HIGHT = 'ОЧЕНЬ ВЫСОКИЙ риск для здоровья. ' \
+                           'Крайне необходимо снижение веса.'
+RECOMENDATION_EXTREMELY_HIGHT = 'ЧРЕЗВЫЧАЙНО ВЫСОКИЙ риск для здоровья. ' \
+                                'Необходимо немедленное снижение массы тела.'
+RECOMENDATION_FOR_MALE = 'Молодой человек, перестаньте играть в игры'
+RECOMENDATION_FOR_FEMALE = 'Юная леди, Вам еще детей рожать'
 
 # формула для расчета ИМТ (делим рост на 100, что бы расчеты были верны)
 bmi = weight / ((height / 100) ** 2)
-
-# 3 варианта рекомендациЙ;)
-if 0 < bmi < 18.5:
-    recomendation = 'У Вас выраженный дефицит массы тела. ' \
-                    'Рекомендуем больше есть'
-elif 18.5 < bmi < 25:
-    recomendation = 'У Вас все ок. Так держать!!!'
-elif bmi > 25:
-    recomendation = 'У Вас ожирение :( Начните меньше кушать.' \
-                    'Запишитесь в тренажерный зал.' \
-                    'Перестаньте играть в компьютерные игры.' \
-                    'Заведите девушку, в конце концов'
 
 # Обращение по полу
 if sex == 'М':
@@ -50,11 +53,55 @@ print('\n====Рекомендация====\n'
       ' - рост', height, '\n'
       ' - вес', weight, '\n\n')
 
-print(recomendation)
+# рекомендации-константы;)
+if 18 < age < 26:
+    if 0 < bmi < 16:
+        if sex == 'М':
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_MALE))
+        else:
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_FEMALE))
+    elif 16 < bmi < 18.5:
+        print(RECOMENDATION_NOT_BAD)
+    elif 18.5 < bmi < 22.9:
+        print(RFECOMENDATION_NORM)
+    elif 23 < bmi < 29.9:
+        print(RECOMENDATION_INCREASED)
+    elif 30 < bmi < 34.9:
+        print(RECOMENDATION_HIGHT)
+    elif 35 < bmi < 39.9:
+        print(RECOMENDATION_VERY_HIGHT)
+    elif bmi > 40:
+        if sex == 'М':
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_MALE))
+        else:
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_FEMALE))
+elif age > 26:
+    if 0 < bmi < 16:
+        if sex == 'М':
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_MALE))
+        else:
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_FEMALE))
+    elif 16 < bmi < 18.5:
+        print(RECOMENDATION_NOT_BAD)
+    elif 18.5 < bmi < 25.9:
+        print(RFECOMENDATION_NORM)
+    elif 26 < bmi < 30.9:
+        print(RECOMENDATION_INCREASED)
+    elif 31 < bmi < 35.9:
+        print(RECOMENDATION_HIGHT)
+    elif 36 < bmi < 40.9:
+        print(RECOMENDATION_VERY_HIGHT)
+    elif bmi > 41:
+        if sex == 'М':
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_MALE))
+        else:
+            print(RECOMENDATION_BAD, '{}'.format(RECOMENDATION_FOR_FEMALE))
+
 
 # построение псевдографика:
 # строка в список. Добавляю "X" в соответствии со значением bmi
 # список в строку
+graph = '0' + '_' * 18 + '18' + '-' * 12 + '30' + '=' * 10 + '40'
 graph = list(graph)
 if int(bmi) > 40:
     graph.append('Х')
